63b05f3504d8c51681f18bcd3a6deb10967843d6
Abhinav-3009 <abhi30092000.ag@gmail.com>
2022-07-29 13:56:00 +0530

    Add project files.

---
+++ ScholarshipPortal/Models/ScholarshipPortalContext.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ScholarshipPortal.Models
+{
+    public partial class ScholarshipPortalContext : DbContext
+    {
+        public ScholarshipPortalContext()
+        {
+        }
+
+        public ScholarshipPortalContext(DbContextOptions<ScholarshipPortalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Institute> Institutes { get; set; }
+        public virtual DbSet<InstituteApproval> InstituteApprovals { get; set; }
+        public virtual DbSet<Scholarship> Scholarships { get; set; }
+        public virtual DbSet<ScholarshipApplication> ScholarshipApplications { get; set; }
+        public virtual DbSet<ScholarshipApproval> ScholarshipApprovals { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=.\\sqlexpress;database=ScholarshipPortal;trusted_connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Institute>(entity =>
+            {
+                entity.ToTable("Institute");
+
+                entity.HasIndex(e => e.Institutecode, "UQ__Institut__0E9926D7C7BCCB36")
+                    .IsUnique();
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.AdmissionStartYear)
+                    .IsRequired()
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.AffiliatedName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.AffiliatedState)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Disecode).HasColumnName("DISECode");
+
+                entity.Property(e => e.District)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.InstituteCategory)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.InstituteType)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Location)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PrincipalName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.State)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<InstituteApproval>(entity =>
+            {
+                entity.ToTable("InstituteApproval");
+
+                entity.HasOne(d => d.Institute)
+                    .WithMany(p => p.InstituteApprovals)
+                    .HasForeignKey(d => d.InstituteId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Institute__Insti__7D439ABD");
+            });
+
+            modelBuilder.Entity<Scholarship>(entity =>
+            {
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<ScholarshipApplication>(entity =>
+            {
+                entity.HasKey(e => e.ApplicationId)
+                    .HasName("PK__Scholars__C93A4C993C178EBE");
+
+                entity.ToTable("ScholarshipApplication");
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.BoardName10th)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.BoardName12th)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ClassStartDate).HasColumnType("date");
+
+                entity.Property(e => e.Community)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DisabilityType)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.District)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fathername)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.InstituteName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IsDisabled)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.MaritalStatus)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ModeOfStudy)
+                    .IsRequired()
+                    .HasMaxLength(7)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Mothername)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ParentsProfession)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PassingYear10th)
+                    .IsRequired()
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PassingYear12th)
+                    .IsRequired()
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PresentCourse)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PresentCourseYear)
+                    .IsRequired()
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PreviousCourse)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PreviousPassingYear)
+                    .IsRequired()
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Religion)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.State)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UniversityBoardName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Institute)
+                    .WithMany(p => p.ScholarshipApplications)
+                    .HasForeignKey(d => d.InstituteId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Scholarsh__Insti__72C60C4A");
+
+                entity.HasOne(d => d.Scholarship)
+                    .WithMany(p => p.ScholarshipApplications)
+                    .HasForeignKey(d => d.ScholarshipId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Scholarsh__Schol__71D1E811");
+
+                entity.HasOne(d => d.Student)
+                    .WithMany(p => p.ScholarshipApplications)
+                    .HasForeignKey(d => d.StudentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Scholarsh__Stude__70DDC3D8");
+            });
+
+            modelBuilder.Entity<ScholarshipApproval>(entity =>
+            {
+                entity.HasKey(e => e.ApprovalId)
+                    .HasName("PK__Scholars__328477F48822B1F5");
+
+                entity.ToTable("ScholarshipApproval");
+
+                entity.HasOne(d => d.Application)
+                    .WithMany(p => p.ScholarshipApprovals)
+                    .HasForeignKey(d => d.ApplicationId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Scholarsh__Appli__787EE5A0");
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("Student");
+
+                entity.HasIndex(e => e.Accountno, "UQ__Student__349C91D47FFDAAAB")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Email, "UQ__Student__A9D10534BBCA8D8A")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Aadhaar, "UQ__Student__C4B33369045C01F6")
+                    .IsUnique();
+
+                entity.Property(e => e.BankIfsc)
+                    .IsRequired()
+                    .HasMaxLength(11)
+                    .IsUnicode(false)
+                    .HasColumnName("BankIFSC");
+
+                entity.Property(e => e.Bankname)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Dob)
+                    .HasColumnType("date")
+                    .HasColumnName("DOB");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(6)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ScholarshipPortal/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ScholarshipPortal.Models
+{
+    public partial class Student
+    {
+        public Student()
+        {
+            ScholarshipApplications = new HashSet<ScholarshipApplication>();
+        }
+
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public DateTime Dob { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public int Institutecode { get; set; }
+        public int Aadhaar { get; set; }
+        public string Bankname { get; set; }
+        public int Accountno { get; set; }
+        public string BankIfsc { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<ScholarshipApplication> ScholarshipApplications { get; set; }
+    }
+}
